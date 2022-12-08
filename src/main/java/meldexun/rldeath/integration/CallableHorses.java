c05de7f2b9ebe3a87b9dbee269ddeb420e75c880
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-12-09 00:04:48 +0100

    Fix clearing of callable horses

--- src/main/java/meldexun/rldeath/integration/CallableHorses.java
+++ src/main/java/meldexun/rldeath/integration/CallableHorses.java
@@ -1,12 +1,25 @@
 package meldexun.rldeath.integration;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import tschipp.callablehorses.common.HorseManager;
+import tschipp.callablehorses.common.capabilities.horseowner.IHorseOwner;
 import tschipp.callablehorses.common.helper.HorseHelper;
 
 public class CallableHorses {
 
 	public static void resetCallableHorses(EntityPlayer player) {
-		HorseHelper.getOwnerCap(player).clearHorse();
+		IHorseOwner horseOwner = HorseHelper.getOwnerCap(player);
+		String ownedID = horseOwner.getStorageUUID();
+		if (!ownedID.isEmpty()) {
+			Entity ent = HorseManager.findHorseWithStorageID(ownedID, player.world);
+			if (ent != null) {
+				HorseManager.clearHorse(HorseHelper.getHorseCap(ent));
+			} else {
+				HorseHelper.getWorldData(player.world).disbandHorse(ownedID);
+			}
+		}
+		horseOwner.clearHorse();
 	}
 
 }
